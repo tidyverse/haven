b1090b0cbadd012f4d03c1fd90da4b51dc79f269
hadley <h.wickham@gmail.com>
2015-02-04 14:52:38 -0600

    Download remote urls. Add example

--- R/haven.R
+++ R/haven.R
@@ -6,8 +6,18 @@ NULL
 #'
 #' @param path Path to file
 #' @export
+#' @examples
+#' read_sas("http://crn.cancer.gov/resources/ctcodes-procedures.sas7bdat")
 read_sas <- function(path) {
-  path <- normalizePath(path, mustWork = TRUE)
-  sas7bdat_df(path)
+  sas7bdat_df(clean_path(path))
 }
 
+clean_path <- function(path) {
+  if (grepl("^(http|ftp|https)://", path)) {
+    tmp <- tempfile()
+    download.file(path, tmp, quiet = TRUE, mode = "wb")
+    tmp
+  } else {
+    normalizePath(path, mustWork = TRUE)
+  }
+}
