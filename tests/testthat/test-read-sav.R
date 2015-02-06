6bd8c9fc8705fd01c11f60f1e4e02a336369280c
hadley <h.wickham@gmail.com>
2015-02-06 09:55:52 -0600

    Test label reading

--- tests/testthat/test-read-sav.R
+++ tests/testthat/test-read-sav.R
@@ -5,3 +5,10 @@ test_that("variable label stored as attributes", {
   expect_equal(attr(df$sex, "label"), "Gender")
 })
 
+test_that("value labels stored as labelled class", {
+  num <- read_sav("labelled-num.sav")
+  str <- read_sav("labelled-str.sav")
+
+  expect_equal(num[[1]], labelled(1, c("This is one" = 1)))
+  expect_equal(str[[1]], labelled(c("M", "F"), c(Female = "F", Male = "M")))
+})
