47e6146160e0ddc0de221e304af3d54698917cad
hadley <h.wickham@gmail.com>
2016-05-31 13:27:49 -0400

    Reorganise labelled tests

--- tests/testthat/test-labelled.R
+++ tests/testthat/test-labelled.R
@@ -1,5 +1,19 @@
 context("Labelled")
 
+test_that("integer and double labels and values are compatible", {
+  expect_error(labelled(1, c(female = 2L, male = 1L)), NA)
+  expect_error(labelled(1L, c(female = 2, male = 1)), NA)
+})
+
+# methods -----------------------------------------------------------------
+
+test_that("printed output is stable", {
+  x <- labelled(1:5, c("Good" = 1, "Bad" = 5))
+  expect_output_file(print(x), "labelled-output.txt")
+})
+
+# zap_labels --------------------------------------------------------------
+
 test_that("zap_labels strips labelled attributes", {
   var <- labelled(c(1L, 98L, 99L),  c(not_answered = 98L, not_applicable = 99L))
   exp <- c(1L,98L,99L)
@@ -9,14 +23,4 @@ test_that("zap_labels strips labelled attributes", {
 test_that("zap_labels returns variables not of class('labelled') unmodified", {
   var <- c(1L, 98L, 99L)
   expect_equal(zap_labels(var), var)
-})
-
-test_that("integer and double labels and values are compatible", {
-  expect_error(labelled(1, c(female = 2L, male = 1L)), NA)
-  expect_error(labelled(1L, c(female = 2, male = 1)), NA)
-})
-
-test_that("printed output is stable", {
-  x <- labelled(1:5, c("Good" = 1, "Bad" = 5))
-  expect_output_file(print(x), "labelled-output.txt")
 })
