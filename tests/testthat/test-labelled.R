e47e62950dd433a1dfa44a9a6b32e3c120e575bd
hadley <h.wickham@gmail.com>
2016-05-31 08:08:24 -0400

    Add regression test for labelled output

--- tests/testthat/test-labelled.R
+++ tests/testthat/test-labelled.R
@@ -15,3 +15,8 @@ test_that("integer and double labels and values are compatible", {
   expect_error(labelled(1, c(female = 2L, male = 1L)), NA)
   expect_error(labelled(1L, c(female = 2, male = 1)), NA)
 })
+
+test_that("printed output is stable", {
+  x <- labelled(1:5, c("Good" = 1, "Bad" = 5))
+  expect_output_file(print(x), "labelled-output.txt")
+})
