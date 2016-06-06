2b49d04a3b7f8f6888fe006b15f4b75c73123a06
hadley <h.wickham@gmail.com>
2016-06-06 16:20:44 -0500

    Move zap_labels into own file

--- tests/testthat/test-labelled.R
+++ tests/testthat/test-labelled.R
@@ -20,21 +20,3 @@ test_that("printed output is stable", {
   x <- labelled(1:5, c("Good" = 1, "Bad" = 5))
   expect_output_file(print(x), "labelled-output.txt")
 })
-
-# zap_labels --------------------------------------------------------------
-
-test_that("zap_labels strips labelled attributes", {
-  var <- labelled(c(1L, 98L, 99L),  c(not_answered = 98L, not_applicable = 99L))
-  exp <- c(1L,98L,99L)
-  expect_equal(zap_labels(var), exp)
-})
-
-test_that("zap_labels returns variables not of class('labelled') unmodified", {
-  var <- c(1L, 98L, 99L)
-  expect_equal(zap_labels(var), var)
-})
-
-test_that("zap_labels is applied to every column in data frame", {
-  df <- tibble::data_frame(x = 1:10, y = labelled(10:1, c("good" = 1)))
-  expect_equal(zap_labels(df)$y, 10:1)
-})
