f3a8f2c7b07a3806786dd2eeb53f04f56c8c23ca
hadley <h.wickham@gmail.com>
2016-06-06 16:27:09 -0500

    Add zap_labels method

--- tests/testthat/test-zap_labels.R
+++ tests/testthat/test-zap_labels.R
@@ -15,3 +15,8 @@ test_that("zap_labels is applied to every column in data frame", {
   df <- tibble::data_frame(x = 1:10, y = labelled(10:1, c("good" = 1)))
   expect_equal(zap_labels(df)$y, 10:1)
 })
+
+test_that("replaces user-defined missings for spss", {
+  x <- labelled_spss(1:5, c(a = 1), na_values = c(2, 4))
+  expect_equal(zap_labels(x), c(1, NA, 3, NA, 5))
+})
