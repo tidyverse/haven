3996c079acc01844b25405b4d13e7f0eefcae9b0
Kristian <kristian@doingit.no>
2016-06-09 22:49:41 +0200

    Fix for #172 (labels) and #177 (variable label).  (#179)
    * Add test for #172. Labels for missing values should be preserved.
    
    * Importing setNames from stats
    
    * Rebased fix for #172 and #177.
    as_factor now preserves variable label. (#177)
    
    as_factor includes and sorts both existing and missing labels. (#172)
    
    * Use stats::setNames instead of importing.
    
    * Added bullets explaining fixes for #172 and #177 to news.

--- tests/testthat/test-as-factor.R
+++ tests/testthat/test-as-factor.R
@@ -11,7 +11,27 @@ test_that("converts characters to factors", {
   expect_equal(as_factor(letters), factor(letters))
 })
 
+test_that("variable label is kept when converting characters to factors (#178)", {
+  s1 <- structure(letters, "label" = "letters")
+  expect_identical(attr(as_factor(s1), "label"), "letters")
+})
+
 # Labelled values ---------------------------------------------------------
+
+test_that("all labels (implicit missing values) are preserved when levels is 'default' or 'both' (#172)", {
+  s1 <- labelled(rep(1, 3), c("A" = 1, "B" = 2, "C" = 3))
+  exp <- factor(rep("A", 3), levels = c("A", "B", "C"))
+  expect_equal(as_factor(s1), exp)
+  
+  exp <- factor(rep("[1] A", 3), levels = c("[1] A", "[2] B", "[3] C"))
+  expect_equal(as_factor(s1, levels = "both"), exp)
+})
+
+test_that("all labels (existing and missing) are sorted by values (#172)", {
+  s1 <- labelled(c(1, 4), c("Agree" = 1, "Neutral" = 2, "Disagree" = 3, "Don't know" = 5))
+  exp <- factor(c("Agree", "4"), levels = c("Agree", "Neutral", "Disagree", "4", "Don't know"))
+  expect_equal(as_factor(s1), exp)
+})
 
 test_that("all values are preserved", {
   s1 <- labelled(1:3, c("A" = 2))
@@ -57,4 +77,11 @@ test_that("labels preserves all label values", {
 test_that("order of labels doesn't matter", {
   var <- labelled(1L, c(female = 2L, male = 1L))
   expect_equal(as_factor(var, "labels"), factor("male", levels = c("female", "male")))
+})
+
+# Variable label
+test_that("variable label is kept when converting labelled to factor (#178)", {
+  s1 <- labelled(1:3, c("A" = 2))
+  attr(s1, "label") <- "labelled"
+  expect_identical(attr(as_factor(s1), "label"), "labelled")
 })
