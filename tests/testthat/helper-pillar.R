1cef1dfe94eda19436ebaace33f2437a6f0945ff
gfellis <gfellis@umn.edu>
2019-01-25 13:07:24 -0600

    Revise tests

--- tests/testthat/helper-pillar.R
+++
@@ -1,5 +0,0 @@
-# Adapted from https://github.com/tidyverse/hms/blob/master/tests/testthat/helper-pillar.R
-expect_known_pillar_shaft_display <- function(x, file, width = 80L) {
-  object_quo <- rlang::quo(pillar::pillar_shaft(x))
-  pillar::expect_known_display(!!object_quo, file = file.path("out", file), width = width)
-}
