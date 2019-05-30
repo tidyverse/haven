f8a3cca0bff625c3bfa65e522d79e17b15a30169
Gorcha (Danny Smith) <danny@gorcha.org>
2019-05-30 18:34:17 +1000

    Add support for additional arguments to roundtrip helpers

--- tests/testthat/helper-roundtrip.R
+++ tests/testthat/helper-roundtrip.R
@@ -1,38 +1,38 @@
 
-roundtrip_sav <- function(x) {
+roundtrip_sav <- function(x, ...) {
   tmp <- tempfile()
   on.exit(unlink(tmp))
 
-  write_sav(x, tmp)
+  write_sav(x, tmp, ...)
   zap_formats(read_sav(tmp))
 }
 
-roundtrip_dta <- function(x) {
+roundtrip_dta <- function(x, ...) {
   tmp <- tempfile()
   on.exit(unlink(tmp))
 
-  write_dta(x, tmp)
+  write_dta(x, tmp, ...)
   zap_formats(read_dta(tmp))
 }
 
-roundtrip_sas <- function(x) {
+roundtrip_sas <- function(x, ...) {
   tmp <- tempfile()
   on.exit(unlink(tmp))
 
-  write_sas(x, tmp)
+  write_sas(x, tmp, ...)
   zap_formats(read_sas(tmp))
 }
 
-roundtrip_xpt <- function(x) {
+roundtrip_xpt <- function(x, ...) {
   tmp <- tempfile()
   on.exit(unlink(tmp))
 
-  write_xpt(x, tmp)
+  write_xpt(x, tmp, ...)
   zap_formats(read_xpt(tmp))
 }
 
 
-roundtrip_var <- function(x, type = "sav") {
+roundtrip_var <- function(x, type = "sav", ...) {
   df <- tibble::tibble(x = x)
 
   # Forces xpt files to be correct length even when ending with
@@ -42,10 +42,10 @@ roundtrip_var <- function(x, type = "sav") {
   }
 
   switch(type,
-    sav = roundtrip_sav(df)$x,
-    dta = roundtrip_dta(df)$x,
-    sas = roundtrip_sas(df)$x,
-    xpt = roundtrip_xpt(df)$x,
+    sav = roundtrip_sav(df, ...)$x,
+    dta = roundtrip_dta(df, ...)$x,
+    sas = roundtrip_sas(df, ...)$x,
+    xpt = roundtrip_xpt(df, ...)$x,
     stop("Unsupported type")
   )
 }
