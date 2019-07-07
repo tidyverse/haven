0b21f214733d613c392b7e81783ed440cec7abc6
Mikko Marttila <mikmart@mikkomarttila.com>
2019-07-07 20:25:55 +0300

    Revert "Use tidyselect's enquo"
    This reverts commit f5f36684d0b8f50f511a2d626425146a67c312a4.

--- R/haven.R
+++ R/haven.R
@@ -42,7 +42,7 @@ read_sas <- function(data_file, catalog_file = NULL,
     warning("`cols_only` is deprecated. Please use `col_select` instead.", call. = FALSE)
     col_select <- cols_only # never allowed tidy selection so no need to enquo
   } else {
-    col_select <- tidyselect::enquo(col_select)
+    col_select <- rlang::enquo(col_select)
   }
 
   if (is.null(encoding)) {
