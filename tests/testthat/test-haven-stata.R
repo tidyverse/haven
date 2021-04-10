40c7cc529384d8cf9ddc59f9f120569de6230e8b
Hadley Wickham <h.wickham@gmail.com>
2021-04-10 09:20:11 -0500

    Centralise Stata tests

--- tests/testthat/test-haven-stata.R
+++ tests/testthat/test-haven-stata.R
@@ -1,3 +1,94 @@
+# read_stata --------------------------------------------------------------
+
+test_that("stata data types read into expected types (#45)", {
+  df <- read_stata("types.dta")
+  types <- vapply(df, typeof, character(1))
+
+  expect_equal(types, c(
+    vfloat = "double",
+    vdouble = "double",
+    vlong = "double",
+    vint = "double",
+    vbyte = "double",
+    vstr = "character",
+    vdate = "double",
+    vdatetime = "double"
+  ))
+})
+
+test_that("Stata %td (date) and %tc (datetime) read into expected classes", {
+  df <- read_stata("types.dta")
+
+  expect_s3_class(df$vdate, "Date")
+  expect_s3_class(df$vdatetime, "POSIXct")
+})
+
+
+test_that("Old %d format read into Date class", {
+  df <- zap_formats(read_stata(test_path("datetime-d.dta")))
+  expect_equal(df$date, as.Date("2015-11-02"))
+})
+
+
+test_that("tagged double missings are read correctly", {
+  x <- read_dta(test_path("tagged-na-double.dta"))$x
+  expect_equal(na_tag(x), c(rep(NA, 5), "a", "h", "z"))
+
+  labels <- attr(x, "labels")
+  expect_equal(na_tag(labels), c("a", "z"))
+})
+
+test_that("tagged integer missings are read correctly", {
+  x <- read_dta(test_path("tagged-na-int.dta"))$x
+  expect_equal(na_tag(x), c(rep(NA, 5), "a", "h", "z"))
+
+  labels <- attr(x, "labels")
+  expect_equal(na_tag(labels), c("a", "z"))
+})
+
+test_that("file label and notes stored as attributes", {
+  df <- read_dta(test_path("notes.dta"))
+
+  expect_equal(attr(df, "label"), "This is a test dataset.")
+  expect_length(attr(df, "notes"), 2)
+})
+
+test_that("only selected columns are read", {
+  out <- read_dta(test_path("notes.dta"), col_select = "id")
+  expect_named(out, "id")
+})
+
+test_that("using skip returns correct number of rows", {
+  rows_after_skipping <- function(n) {
+    nrow(read_dta(test_path("notes.dta"), skip = n))
+  }
+
+  n <- rows_after_skipping(0)
+
+  expect_equal(rows_after_skipping(1), n - 1)
+  expect_equal(rows_after_skipping(n - 1), 1)
+  expect_equal(rows_after_skipping(n + 0), 0)
+  expect_equal(rows_after_skipping(n + 1), 0)
+})
+
+test_that("can limit the number of rows to read", {
+  rows_with_limit <- function(n) {
+    nrow(read_dta(test_path("notes.dta"), n_max = n))
+  }
+
+  n <- rows_with_limit(Inf)
+  expect_equal(rows_with_limit(0), 0)
+  expect_equal(rows_with_limit(1), 1)
+  expect_equal(rows_with_limit(n), n)
+  expect_equal(rows_with_limit(n + 1), n)
+
+  # alternatives for unlimited rows
+  expect_equal(rows_with_limit(NA), n)
+  expect_equal(rows_with_limit(-1), n)
+})
+
+# write_dta ---------------------------------------------------------------
+
 test_that("can roundtrip basic types", {
   x <- runif(10)
   expect_equal(roundtrip_var(x, "dta"), x)
@@ -76,7 +167,6 @@ test_that("can write labelled with NULL labels", {
   expect_equal(roundtrip_var(chr, "dta"), c("a", "b"))
 })
 
-
 test_that("factors become labelleds", {
   f <- factor(c("a", "b"), levels = letters[1:3])
   rt <- roundtrip_var(f, "dta")
@@ -148,3 +238,4 @@ test_that("throws error for invalid file labels", {
   expect_error(write_dta(df, tempfile(), label = paste(rep("a", 100), collapse = "")),
                "data labels must be 80 characters or fewer")
 })
+
