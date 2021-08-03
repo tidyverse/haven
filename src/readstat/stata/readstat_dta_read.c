4a878a10a290a164c24f30edfb7e5192b1f15b1d
Hadley Wickham <h.wickham@gmail.com>
2021-08-03 13:38:24 -0500

    Hack in a solaris fix

--- src/readstat/stata/readstat_dta_read.c
+++ src/readstat/stata/readstat_dta_read.c
@@ -1,6 +1,3 @@
-
-#define _XOPEN_SOURCE 700 /* for strnlen */
-
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
@@ -8,6 +5,16 @@
 #include <string.h>
 #include <inttypes.h>
 #include <sys/types.h>
+
+#if !defined(_POSIX_VERSION) || _POSIX_VERSION < 200809L
+size_t strnlen(const char* s, size_t maxlen) {
+  const char* end;
+  end = memchr(s, '\0', maxlen);
+  if (end == NULL)
+    return maxlen;
+  return end - s;
+}
+#endif
 
 #include "../readstat.h"
 #include "../readstat_bits.h"
