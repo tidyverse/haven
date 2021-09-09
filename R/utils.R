bff47e53434ea26f2dc57fe71fbe0a4f69e5fa8b
Danny Smith <danny@gorcha.org>
2021-09-09 11:16:16 +1000

    Clean up formats in tz conversion in `force_utc()`

--- R/utils.R
+++ R/utils.R
@@ -13,7 +13,8 @@ force_utc <- function(x) {
     x
   } else {
     x_attr <- attributes(x)
-    x <- as.POSIXct(format(x, usetz = FALSE), tz = "UTC", tryFormats = c("%Y-%m-%d %H:%M:%S", "%Y-%m-%d"))
+    x <- as.POSIXct(format(x, usetz = FALSE, format = "%Y-%m-%d %H:%M:%S"),
+                    tz = "UTC", format = "%Y-%m-%d %H:%M:%S")
     attr_miss <- setdiff(names(x_attr), c(names(attributes(x)), "names"))
     attributes(x)[attr_miss] <- x_attr[attr_miss]
     x
